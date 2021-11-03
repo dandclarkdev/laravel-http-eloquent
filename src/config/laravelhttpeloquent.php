83c4d72cee4e4b16869512a693f5ad69aef02299
danclarkdev <danclarkdev@gmail.com>
2021-11-03 16:50:20 +0000

    config

--- src/config/laravelhttpeloquent.php
+++ src/config/laravelhttpeloquent.php
@@ -1,14 +1,9 @@
 <?php
 
-use App\Models\Comment;
-
 return [
     'services' => [
         'placeholder' => [
             'base_url' => 'https://jsonplaceholder.typicode.com',
-            'models' => [
-                'comments' => Comment::class
-            ]
         ]
     ]
 ];
