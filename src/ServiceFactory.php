72ec0e3c9d49c1c90129b4767991981538baaf1f
danclarkdev <danclarkdev@gmail.com>
2021-11-04 17:46:44 +0000

    Adding fallback service

--- src/ServiceFactory.php
+++ src/ServiceFactory.php
@@ -2,35 +2,9 @@
 
 namespace LaravelHttpEloquent;
 
-use HttpEloquent\Types\BaseUrl;
-use HttpEloquent\Types\ModelMap;
 use LaravelHttpEloquent\Service;
-use HttpEloquent\Types\ServiceConfig;
 use HttpEloquent\ServiceFactory as BaseServiceFactory;
-use HttpEloquent\Interfaces\Service as ServiceInterface;
-use HttpEloquent\Types\WrapperProperty;
-
 class ServiceFactory extends BaseServiceFactory
 {
-    public function make(string $serviceName): ServiceInterface
-    {
-        $config = $this->configProvider->getConfig(
-            $serviceName
-        );
-
-        return new Service(
-            new ServiceConfig(
-                new BaseUrl(
-                    $config['base_url']
-                ),
-                new ModelMap(
-                    $config['models'] ?? []
-                ),
-                isset($config['wrapper']) ? new WrapperProperty(
-                    $config['wrapper']
-                ) : null
-            ),
-            $this->getClient()
-        );
-    }
+    protected const FALLBACK_SERVICE_CLASS = Service::class;
 }
